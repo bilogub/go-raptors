39c5454c5c5be360ac85083d0ef72f4e3e65a447
Harry Lewis <harry.lewis@queensu.ca>
2019-11-08 21:03:03 -0500

    Add Status to Response

--- raptors.go
+++ raptors.go
@@ -23,28 +23,35 @@ type Team struct {
 	Score        string `json:"score"`
 }
 
+// Period, time, game status
+type PeriodTime struct {
+	GameStatus  string `json:"game_status"`
+	Period      string `json:"period_value"`
+	Clock       string `json:"game_clock"`
+}
+
 // GameRecord game scheduled
 type GameRecord struct {
-	ID               int    `json:"id"`
-	Date             string `json:"date"`
-	Time             string `json:"time"`
-	Arena            string `json:"arena"`
-	City             string `json:"city"`
-	State            string `json:"state"`
-	Country          string `json:"country"`
-	HomeStartDate    string `json:"home_start_date"`
-	HomeStartTime    string `json:"home_start_time"`
-	VisitorStartDate string `json:"visitor_start_date"`
-	VisitorStartTime string `json:"visitor_start_time"`
-	Home             Team   `json:"home"`
-	Visitor          Team   `json:"visitor"`
-	Period           int    `json:"period"`
-	Postseason       bool   `json:"postseason"`
-	Season           int    `json:"season"`
-	Status           string `json:"status"`
-	VisitorTeamScore int    `json:"visitor_team_score"`
-	IsHomeTeam       int    `json:"is_home_team"`
-	Outcome          string `json:"outcome"`
+	ID               int        `json:"id"`
+	Date             string     `json:"date"`
+	Time             string     `json:"time"`
+	Arena            string     `json:"arena"`
+	City             string     `json:"city"`
+	State            string     `json:"state"`
+	Country          string     `json:"country"`
+	HomeStartDate    string     `json:"home_start_date"`
+	HomeStartTime    string     `json:"home_start_time"`
+	VisitorStartDate string     `json:"visitor_start_date"`
+	VisitorStartTime string     `json:"visitor_start_time"`
+	Home             Team       `json:"home"`
+	Visitor          Team       `json:"visitor"`
+	Period           int        `json:"period"`
+	Postseason       bool       `json:"postseason"`
+	Season           int        `json:"season"`
+	Status           PeriodTime `json:"period_time"`
+	VisitorTeamScore int        `json:"visitor_team_score"`
+	IsHomeTeam       int        `json:"is_home_team"`
+	Outcome          string     `json:"outcome"`
 }
 
 // Response - JSON Body
